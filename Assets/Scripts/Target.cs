c6e31a1fc9531d6aa1d8023dbebc1a34278eb95e
vahkd33114 <vahkd33114@yahoo.co.jp>
2025-07-01 00:16:33 +0900

    フライパンの移動処理

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,24 +1,29 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
     [SerializeField] private GameObject targetPrefab;
-
+    [SerializeField] private Transform[] _target;
+    [SerializeField] private float _moveSpeed = 1f;
+    [SerializeField] float _stoppingDistance = 0.05f;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    int _currentTargetIndex = 0;
 
-    // Update is called once per frame
     void Update()
     {
-        
-    }
+        float distance = Vector2.Distance(this.transform.position, _target[_currentTargetIndex].position);
+        if (distance > _stoppingDistance)
+        {
+            Vector3 dir = (_target[_currentTargetIndex].transform.position - this.transform.position).normalized *
+                          _moveSpeed;
+            this.transform.Translate(dir * Time.deltaTime);
+        }
+        else
+        {
+            _currentTargetIndex++;
+            _currentTargetIndex %= _target.Length;
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        
+        }
     }
 }
