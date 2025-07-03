ec451b41ad298b1407c52ac715af849befe03054
vahkd33114 <vahkd33114@yahoo.co.jp>
2025-07-03 14:27:42 +0900

    効果音

--- Assets/Scripts/Scene.cs
+++ Assets/Scripts/Scene.cs
@@ -1,13 +1,31 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 //　シーンの遷移を行う
 public class Scene : MonoBehaviour
 {
-    [SerializeField] private float delaySeconds = 20f;// 遷移までの待機時間
-    [SerializeField] private string NextSceneName = "Result";// 遷移先のシーン名
-    public void ChangeInGameScene()
+    [SerializeField] private float delaySeconds = 2f;// 遷移までの待機時間
+    [SerializeField] public AudioClip audioClip;
+    private AudioSource audioSource; // InspectorからAudioSourceを設定
+
+    void Start()
     {
+        // もしAudioSourceがInspectorが設定されていなければ、自身から取得
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
+    public void OnClick()
+    {
+        PlaySound();
+        StartCoroutine(ChangeInGameScene());
+    }
+    private IEnumerator ChangeInGameScene()
+    {
+        yield return new WaitForSeconds(delaySeconds);
         SceneManager.LoadScene("InGame");
     }
     
@@ -19,5 +37,12 @@ public class Scene : MonoBehaviour
     public void ChangeStartScene()
     {
         SceneManager.LoadScene("Title");
+    }
+    public void PlaySound()
+    {
+        if (audioSource != null && audioSource.clip != null)
+        {
+            audioSource.PlayOneShot(audioClip); // AudioClipを再生
+        }
     }
 }
